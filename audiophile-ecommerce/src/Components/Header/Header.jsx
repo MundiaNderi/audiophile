b3a9f0058b9f7dec6cc09079a4f6f3875813dadd
Serah Nderi <sarahngima77@gmail.com>
2024-04-02 22:19:33 +0300

    Fixing the header image

--- audiophile-ecommerce/src/Components/Header/Header.jsx
+++ audiophile-ecommerce/src/Components/Header/Header.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import SeeProduct from '../Button/SeeProduct'
+import React from 'react';
+import SeeProduct from '../Button/SeeProduct';
+import Headphones from '/image-hero.jpg';
+import MobileHeadphones from '/image-header.jpg'
 
-import './Header.css'
 const Header = () => {
   return (
-    <div className='header bg-black px-4 md:px-32 flex justify-between items-center pt-4 pb-8' >
-        <div className=' text-white justify-center items-center pr-8'>
-            <p className='font-manrope pb-3 font-thin tracking-widest text-sm' >NEW PRODUCT</p>
-            <h1 className='font-manrope pb-3 text-3xl  '>XX99 Mark II <br />Headphones</h1>
-            <p className='font-manrope pb-3 text-md ' >Experience natural, lifelike audio and exceptional
-                build quality made for the passionate music enthusiast
-            </p>
-            <SeeProduct />
+    <div className='flex  md:flex-row bg-black pt-4 md:px-32'>
+      <div className='md:w-1/2 flex items-center md:pr-4 '>
+        <div className='md:hidden relative'>
+          <img src={MobileHeadphones} className='w-full h-1/3  ' alt='Headphones' />
         </div>
-        <div className='' >
+        <div className='flex flex-col items-center  md:items-start absolute md:ml-8'>
+          <p className='text-white font-bold font-manrope'>NEW PRODUCT</p>
+          <h1 className='text-white font-bold pt-2 '>
+            XX99 Mark II Headphones
+          </h1>
+          <p className='text-white text-center md:text-left font-manrope pt-2 pb-3 '>
+            Experience natural, lifelike audio and exceptional build
+            quality made for the passionate music enthusiast.
+          </p>
+          <SeeProduct className='' />
         </div>
+      </div>
+      <div className='hidden md:block md:w-1/2'>
+        <img src={Headphones} className='w-full' alt='Headphones' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
