54f29ced22924635a05ff650bf072f9b0696324c
Serah Nderi <sarahngima77@gmail.com>
2025-05-02 12:07:54 +0300

    Fixing ports

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,34 @@
 // Module express server
-import express from 'express'
-import cors from 'cors'
-import { connectDB } from './config/db.js'
-import dotenv from 'dotenv';
-import audioRouter from './routes/audioRoute.js';
-import userRouter from './routes/userRoute.js';
-
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import audioRouter from "./routes/audioRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 dotenv.config();
 
 // app config
-const app = express()
-const port = 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use('/api/audio', audioRouter)
-app.use('/uploads', express.static('uploads'))
-app.use('/api/user', userRouter)
+app.use("/api/audio", audioRouter);
+app.use("/uploads", express.static("uploads"));
+app.use("/api/user", userRouter);
 
 // http method to request data from the server
 app.get("/", (req, res) => {
-    res.send("API Working")
-})
+  res.send("API Working");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
+  console.log(`Server is running on port ${port}`);
+});
