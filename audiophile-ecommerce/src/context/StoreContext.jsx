b13fa3b26390cfa733297ef5eb1208236f6839f0
Serah Nderi <sarahngima77@gmail.com>
2025-05-02 19:27:20 +0300

    Connecting the frontend to the render backend

--- audiophile-ecommerce/src/context/StoreContext.jsx
+++ audiophile-ecommerce/src/context/StoreContext.jsx
@@ -12,7 +12,7 @@ const StoreContextProvider = (props) => {
         const fetchData = async () => {
             try {
                 //const response = await axios.get("https://localhost:4000/api/audio/list");
-                const response = await axios.get("https://audiophile-ecommerce-api.onrender.com");
+                const response = await axios.get("https://audiophile-api-qtz6.onrender.com");
                 console.log(response.data.data);
                 setProductData(response.data.data);
             } catch (error) {
