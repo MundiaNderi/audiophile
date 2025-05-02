2e39ea1cfabbfa0d56070c8767278c119a609f98
Serah Nderi <sarahngima77@gmail.com>
2025-05-02 12:15:09 +0300

    Fixing Vite config

--- audiophile-ecommerce/vite.config.js
+++ audiophile-ecommerce/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Binds to all network interfaces (necessary for Render)
+    port: 4173, // Ensure this is an available port
+  },
+  preview: {
+    port: 4173, // Same as above for consistency
+  },
+});
